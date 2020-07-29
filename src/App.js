3d45d2c5ebe0a6780dca3eadaee89ca67e7cc4f3
js250141 <joseph.sheridan@teradata.com>
2020-07-29 10:30:58 -0700

    updated travis file

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          I was changed!
+          To get started, edit src/App.js and save to reload.
         </a>
       </header>
     </div>
