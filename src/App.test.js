263a0474990cb7c5dcb2fb9df11f0b35eb6dacb4
js250141 <joseph.sheridan@teradata.com>
2020-07-29 10:11:10 -0700

    updated travis file

--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('I was changed!', () => {
+test('renders learn react link', () => {
   const { getByText } = render(<App />);
   const linkElement = getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
